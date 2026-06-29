7b3a07679a9298cc4a02f8d731ed5d5c0a072746
mahedizaman <info.zaman.profile@gmail.com>
2026-06-29 15:14:29 +0600

    fiat: create a dart project for weather app

---
+++ lib/material_design_weatherapp.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WeatherAppMaterialPage extends StatefulWidget {
+  const WeatherAppMaterialPage({super.key});
+
+  @override
+  State<WeatherAppMaterialPage> createState() => _WeatherAppMaterialPageState();
+}
+
+class _WeatherAppMaterialPageState extends State<WeatherAppMaterialPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
